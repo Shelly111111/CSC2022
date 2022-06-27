33d8f0b3bd57ae31a894c70ffab7fa10d43a1d82
戈 张 <921519274@qq.com>
2022-06-27 13:01:05 +0800

    add sql

---
+++ sql/changedetection1.sql
@@ -0,0 +1,30 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : test
+ Source Server Type    : MySQL
+ Source Server Version : 80029
+ Source Host           : localhost:3306
+ Source Schema         : updata
+
+ Target Server Type    : MySQL
+ Target Server Version : 80029
+ File Encoding         : 65001
+
+ Date: 27/06/2022 12:46:41
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for changedetection1
+-- ----------------------------
+DROP TABLE IF EXISTS `changedetection1`;
+CREATE TABLE `changedetection1`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `imgSrc` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
